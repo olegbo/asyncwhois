7a5d573e3c25edd5705e7b006e27aa220991ede4
Oleg Bogumirski <obogumirski@gmail.com>
2023-02-26 01:11:59 +0300

    ' -> "

--- asyncwhois/query.py
+++ asyncwhois/query.py
@@ -134,7 +134,7 @@ class Query:
             # parse response for the referred WHOIS server name
             whois_server = self._find_match(regex, query_output)
             whois_server = whois_server.lower()
-            if whois_server and whois_server != '' and whois_server != server:
+            if whois_server and whois_server != "" and whois_server != server:
                 # recursive call to find more authoritative server
                 query_output = self._do_query(
                     whois_server, data, self.whois_server_regex
